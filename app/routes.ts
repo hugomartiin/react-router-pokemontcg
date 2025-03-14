65cbf667befc50a07f23542b136ef47d8725f084
Barrera <alumno.138627@ies-azarquiel.es>
2025-03-14 11:30:52 +0100

    final v2

--- app/routes.ts
+++ app/routes.ts
@@ -2,21 +2,6 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
 
-    //Para testear
-    // layout("layouts/footer.tsx", [
-
-    //     layout("layouts/header.tsx", [
-    //         index("routes/home.tsx"),
-    //             route("cards","routes/cards.tsx"),
-    //             route("favourites","routes/favourites.tsx"),
-    //             route("series","routes/series.tsx"),
-    //             route("sets","routes/sets.tsx"),
-    //     ]),
-    // ])
-
-    
-    //Rutas finales
-
     index("routes/home/home.tsx"),
     layout("layouts/mainLayout.tsx", [
         route("filters/:setId?","routes/filters.tsx"),
